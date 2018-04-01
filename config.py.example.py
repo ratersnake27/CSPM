9d09ef26c6846abe2eb60e8d2fc22292e1202355
RobTwoThree <rsayson31@gmail.com>
2018-04-01 15:54:06 -0700

    Added a sample config file

---
+++ config.py.example.py
@@ -0,0 +1,14 @@
+bot_channel = '12345' #limit users to using the bot in this channel, right click and copy ID.
+token= 'XXXX12345' #discord bot token (NOT bot ID)
+website = 'http://mapnamehere.com' #URL to map if you want the .map function to work, simply gives the users the website
+log_channel = '01234' #Add a log channel, copy the ID here, give this bot access to the channel.
+
+
+#####DATABASE, limited to MySQL and MariaDB#####
+host = 'localhost'  #host
+user = 'username'  #database user
+password = 'password'  #database password
+database = 'database_name'  #database name
+
+########Custom Fields#########
+instance_id = 'instance_name'
